5ef7b04c45c59373449903fe0c55545d0c5f7d7a
Akwesi-Bona <arhinbonnah@gmail.com>
2024-04-10 22:36:05 +0000

    console error fixed

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -15,7 +15,11 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
   const handleClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
-  const vote = () => {setVotes({...votes, [selected]: votes[selected] + 1})}
+  const vote = () => {
+    const newVotes = [...votes]
+    newVotes[selected]++
+    setVotes(newVotes)
+  }
 
   return (
     console.log(votes),
